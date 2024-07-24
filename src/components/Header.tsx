3bf793f3512fe7d03d3bac2a8f15b5aa4bc6b7a4
pbowerre <pbowerre@gmail.com>
2024-07-24 01:21:45 +0100

    updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ function Header() {
       <nav className="navbar">
         <div className="logo">LinksCorp</div>
         <ul className="nav-links">
-          <li><a href="#product">Join Now</a></li>
+          <li><Link to={"/banktransfer"} className='Linkers'>Join Now</Link></li>
         </ul>
       </nav>
     </header>
