642ea4272b3d3714d2766dae7c987f6feef3eb78
pbowerre <pbowerre@gmail.com>
2024-07-25 11:42:41 +0100

    updated

--- src/components/Owner.tsx
+++ src/components/Owner.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
+import '../styles/Owner.css'
 const Owner = () => {
   return (
-    <div>Owner</div>
+    <div className='ownerWrap'>
+            <h1>FinanX.Defi</h1>
+        <div className="ownerWrapInner">
+            <h3>I've coached over 30,000+ students and counting, I'm determined to sharing my knowledge and skill almost for free, in order to make an impact to our society.</h3>
+            <img src="../src/assets/owner.jpg" alt="Finanx Logo" />
+            <div className="title">
+                <span>Finanx.Defi</span>
+                <span>Defi Hunter</span>
+            </div>
+        </div>
+    </div>
   )
 }
 
