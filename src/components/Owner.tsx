58f3cf14119569c15146d8f4d6d75ee8ed165ccb
pbowerre <pbowerre@gmail.com>
2024-07-26 02:24:08 +0100

    image updated

--- src/components/Owner.tsx
+++ src/components/Owner.tsx
@@ -6,7 +6,6 @@ const Owner = () => {
             <h1>FinanX.Defi</h1>
         <div className="ownerWrapInner">
             <h3>I've coached numerous students and still counting, I'm determined to sharing my knowledge and skill almost for free, in order to make an impact to our society.</h3>
-            <img src="../src/assets/owner.jpg" alt="Finanx Logo" />
             <div className="title">
                 <span>Defi Hunter</span>
             </div>
