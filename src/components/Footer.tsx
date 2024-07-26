1d1a11c655a15a8d31e0a1c275f7349b9a3d2f41
pbowerre <pbowerre@gmail.com>
2024-07-26 01:47:12 +0100

    updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,17 +9,17 @@ const Footer = () => {
         <ul>
             <li>
                 <a href="https://x.com/i/lists/1734379877099360479?t=1k86LSd7exJ7fxL2hG-N4Q&s=09">
-                <img src='../src/assets/twitter.png' alt='twitter logo'/>
+                <img src='https://static.vecteezy.com/system/resources/previews/027/395/710/non_2x/twitter-brand-new-logo-3-d-with-new-x-shaped-graphic-of-the-world-s-most-popular-social-media-free-png.png' alt='twitter logo'/>
                 </a>
             </li>
             <li>
                 <a href="https://chat.whatsapp.com/EMWNkk0ebB3BgBWA276hsU">
-                <img src='../src/assets/whatsapp.png' alt='whatsapp logo'/>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/598px-WhatsApp_icon.png?20200503174721' alt='whatsapp logo'/>
                 </a>
             </li>
             <li>
                 <a href="https://t.me/Crypto_CircleC">
-                    <img src='../src/assets/telegram.png' alt='telegram logo'/>
+                    <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Telegram_font_awesome.svg/1024px-Telegram_font_awesome.svg.png' alt='telegram logo'/>
                 </a>
             </li>
 
--- src/components/Owner.tsx
+++ src/components/Owner.tsx
@@ -5,7 +5,7 @@ const Owner = () => {
     <div className='ownerWrap'>
             <h1>FinanX.Defi</h1>
         <div className="ownerWrapInner">
-            <h3>I've coached over 30,000+ students and counting, I'm determined to sharing my knowledge and skill almost for free, in order to make an impact to our society.</h3>
+            <h3>I've coached numerous students and still counting, I'm determined to sharing my knowledge and skill almost for free, in order to make an impact to our society.</h3>
             <img src="../src/assets/owner.jpg" alt="Finanx Logo" />
             <div className="title">
                 <span>Defi Hunter</span>
